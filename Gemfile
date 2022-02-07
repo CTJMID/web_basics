78e57d1ced40e1a436de693756b5822389c059ab
Bromley <52469112+nyahehnagi@users.noreply.github.com>
2022-02-07 15:44:07 +0000

    Part 8 Returning HTML
    Co-authored-by: Chris Middleton <CTJMID@users.noreply.github.com>

--- Gemfile
+++ Gemfile
@@ -6,5 +6,6 @@ ruby '3.0.0'
 
 gem "rspec"
 gem "sinatra"
+gem "sinatra-contrib"
 gem 'capybara'
 gem 'webrick'
--- app.rb
+++ app.rb
@@ -1,5 +1,23 @@
 require 'sinatra'
+require "sinatra/reloader" if development?
 
 get '/' do
   "hello!"
 end
+
+get '/secret' do
+  "this is a secret"
+end
+
+get '/testing' do
+  "this is a test"
+end
+
+get '/cat' do
+  "
+  <div style='border: dashed red'>
+    <img src='https://i.imgur.com/jFaSxym.png'>
+  </div>
+  "
+end
+
